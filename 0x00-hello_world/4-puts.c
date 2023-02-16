371f80756af358de81b75934771535237edfc817
Clemuch009 <clementmuchaimoko@gmail.com>
2023-02-16 06:58:03 -0800

    printers

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * file: 4-puts.c
+ * author: clement
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints Programming is like building
+ *	a multilingual puzzle
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * file : 5-printf.c
+ * author : clement
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints 'with proper grammar, but
+ *	the outcome is a piece of art,'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
