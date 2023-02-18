2d72c1256984bb853ff6042d40072201acf96aa7
Clemuch009 <clementmuchaimoko@gmail.com>
2023-02-18 13:17:43 -0800

    programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+/*
+ * file4
+ * author ;clement
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main -prints the alphabet in lowercase
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+/*
+ * file8
+ * clemen
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers
+ *  of base 16 in lowercase
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	int number;
+	char letter;
+
+	{
+		for (number = 0; number < 10; number++)
+			putchar((number % 10) + '0');
+
+		for (letter = 'a'; letter <= 'f'; letter++)
+
+			putchar('\n');
+	}
+
+		return (0);
+}
