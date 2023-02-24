a325ffb5fe5599da9c4ded114962bd54a1b152a9
Clemuch009 <clementmuchaimoko@gmail.com>
2023-02-24 05:49:30 -0800

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,8 +14,17 @@
  */
 void print_number(int n)
 {
-	if (n >= '0' && n  < '0')
-		_putchar((n % 10) + '0');
 
+	unsigned int num = n;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
