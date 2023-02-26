e4fa488d1f6391772874a2b549192c176f356884
Clemuch009 <clementmuchaimoko@gmail.com>
2023-02-25 18:16:19 -0800

    c+

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+/*
+ * file1
+ * write
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main -prints all possible combinations of two two
+ * -digit numbers
+ *
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for{"i == "0" && i =< "99" i++)
+		printf(" % 2d \n");
+
+		return (0);
+}
