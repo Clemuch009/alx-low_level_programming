9d3820e291e9f316fd98ecc3a297907637c65b8e
Clemuch009 <clementmuchaimoko@gmail.com>
2023-03-02 14:00:42 -0800

    code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,15 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int let = 0, dest_num = 0;
 
-	while (dest[let++])
-		dest_num++;
+	int index = 0, dest_len = 0;
 
-	for (let = 0; src[let]; let++)
-		dest[dest_num++] = src[let];
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
+
