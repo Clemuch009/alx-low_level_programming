318cbfc5f5693ecf6bfc4f80dfa624e003d32e81
Clemuch009 <clementmuchaimoko@gmail.com>
2023-03-02 13:50:03 -0800

    code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,13 +16,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int let = 0, dest_num = 0;
 
-	while (dest[let++])
-		dest_num++;
+	int index = 0, dest_len = 0;
 
-	for (let = 0; src[let] && let < n; let++)
-		dest[dest_let++] = src[let];
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
