17c7b5b7595b22c11205bd67eab9360934f3fe5c
Clemuch009 <clementmuchaimoko@gmail.com>
2023-03-06 13:03:05 -0800

    code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,10 @@
 
 /**
  * _memcpy - copies memory are
- * * @dest: A pointer to the memory area to copy @src into.
+ *  @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
  * @n: The number of bytes to copy from @src.
  *
- * Return: A pointer to the destination buffer @dest.
  */
 void *_memcpy(void *dest, const void *src, size_t n)
 {
@@ -23,7 +22,5 @@ void *_memcpy(void *dest, const void *src, size_t n)
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
-
-	return (dest);
 }
   
