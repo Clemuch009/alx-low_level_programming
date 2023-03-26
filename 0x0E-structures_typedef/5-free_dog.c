ad78a19cc359c72b38408d52fc6a2f422d8aa1d1
Clemuch009 <clementmuchaimoko@gmail.com>
2023-03-26 04:23:24 -0700

    code

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+/*
+ * file5
+ * author
+ *
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
