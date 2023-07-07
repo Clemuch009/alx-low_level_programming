0b7b35dd49411471e06d22d0a675f69e577eb9b0
root <clementmuchaimoko@gmil.com>
2023-07-07 12:23:41 -0700

    code

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,16 +6,20 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit
- * @n: integer to grab
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
  *
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 32)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-	return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
