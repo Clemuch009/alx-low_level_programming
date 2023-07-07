8efa0e83cb1b3d6d13b01209a8e1b0a48fd384c9
root <clementmuchaimoko@gmil.com>
2023-07-07 11:08:05 -0700

    code

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+/**
+ * file1
+ * author
+ */
+
+#include "main.h"
+
+/**
+ * binary_to_uint - binary to uint
+ * @b:  converted number
+ * Return: unsigned int
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int y = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b == '1')
+			y = (y << 1) | 1;
+		else if (*b == '0')
+			y <<= 1;
+		else
+			return (0);
+		b++;
+	}
+	return (y);
+}
+
