3563911b7495ee5b9f963d0137b161810e96190e
Clemuch009 <clementmuchaimoko@gmail.com>
2025-08-30 02:09:05 -0700

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 /*
- * Prints size of various types on the computer it is compiled and run on.
+ *Prints size of various types on the computer it is compiled and run on.
  *
  */
+
 #include <stdio.h>
+
 int main(void)
 {
-	printf("Size of a char :%ld bytes\n", sizeof(char));
-	printf("Size of a int :%ld bytes\n", sizeof(int));
-	printf("Size of a long int :%ld bytes\n", sizeof(long int));
-	printf("Size of a long long int :%ld bytes\n", sizeof(long long int));
-	printf("Size of a float :%ld bytes\n", sizeof(float));
-
-	return (0);
+	    printf("Size of a char: %ld bytes\n", sizeof(char));
+	    printf("Size of an int: %ld bytes\n", sizeof(int));
+	    printf("Size of a long int: %ld bytes\n", sizeof(long int));
+	    printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
+	    printf("Size of a float: %ld bytes\n", sizeof(float));
+	    
+	    return (0);
 }
+
