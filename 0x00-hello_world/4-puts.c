11ef1fbfe3a11ace04b86d4d4879c1388e268852
Clemuch009 <clementmuchaimoko@gmail.com>
2025-08-30 02:14:38 -0700

    cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 /*
  *Prints "Programming is like building a multilingual puzzle".
+ * main() is the entry point of the program. It calls the `puts` function
+ * to print the specified message to the standard output (the console).
  */
 
 #include <stdio.h>
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,");
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 }
