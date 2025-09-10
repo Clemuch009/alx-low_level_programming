9fed89dc32e4bace860c1edc390d997e5aaca710
Clemuch009 <clementmuchaimoko@gmail.com>
2025-09-10 22:26:09 +0300

    argument_to_main

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,10 +16,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-
-	printf("%d\n", a * b);
+	printf("%d\n", atoi(argv[1]) *  atoi(argv[2]));
 	return (0);
 }
 
